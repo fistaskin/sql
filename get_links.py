1abb415419366bc441f50e1f7e2e7ca10f4c4cca
fistaskin <31420373+fistaskin@users.noreply.github.com>
2022-10-17 14:27:59 +0300

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -1,34 +1,32 @@
-﻿from selenium import webdriver
-import os
+# -*- coding: utf-8 -*-
 
+from selenium import webdriver
+import os
 
 d = webdriver.Chrome()
 os.system('timeout 10')
-page = 0
+page = 370
 while(1):
- hosts = open('results//'+str(page)+'.txt', 'w')
- d.get('https://go.mail.ru/search?fm=1&q=%D0%BF%D0%BE%D1%80%D0%BD%D0%BE&sf='+str(page))
- os.system('timeout 10')
+ try:
+  hosts = open('C://OSPanel//domains//new//results//'+str(page)+'.txt', 'w')
+  d.get('https://go.mail.ru/search?fm=1&q=фильмы онлайн&sf='+str(page))
+  os.system('timeout 10')
 
- results = d.find_elements_by_tag_name('li')
+  results = d.find_element_by_class_name('SERP-result')
+  links = []
+  result = results.find_elements_by_tag_name('li')
 
- links = []
-
- i = 6
-
- for x in results:
-  a = results[i].find_element_by_tag_name('a').get_attribute('href')
-  links.append(a)
-  i = i + 1
-  if i == 18:
-   break
+  for x in result:
+   link = x.find_element_by_tag_name('a').get_attribute('href')
+   links.append(link)
+  
  
- for x in links:
-  hosts.write(x)
-  hosts.write('\n')
+  for x in links:
+   hosts.write(x)
+   hosts.write('\n')
 
- hosts.close()
+  hosts.close()
 
- page = page + 10
- if page == 620:
-  break
+  page = page + 10
+ except:
+  exit()
